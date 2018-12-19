6be05fb01cb26353516bffddd11c14a039fdcc8a
David Glass <wireduk@gmail.com>
2018-12-19 23:47:59 +0000

    Initial commit

---
+++ HealthCheckDemo/HealthChecks/FolderExistsHealthCheck.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckDemo.HealthChecks
+{
+    public class FolderExistsHealthCheck : IHealthCheck
+    {
+        private readonly string _folder;
+
+        public FolderExistsHealthCheck(string folder)
+        {
+            _folder = folder;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            await Task.CompletedTask;
+
+            return Directory.Exists(_folder)
+                ? HealthCheckResult.Healthy()
+                : HealthCheckResult.Unhealthy();
+        }
+    }
+}
